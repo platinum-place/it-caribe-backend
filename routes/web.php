c52cfb9e615a460c06a44a8899c369d4ae1a0a20
warlyn <warlyn200@gmail.com>
2025-06-24 14:23:11 -0400

    update limit

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,6 @@
 use Filament\Http\Middleware\Authenticate;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return redirect('/admin');
-});
 
 Route::middleware(Authenticate::class)
     ->prefix('filament/resources')
