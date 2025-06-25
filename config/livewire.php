d23743765c00ee129b0ad6b460ac637406b38bba
warlyn <warlyn200@gmail.com>
2025-06-25 11:29:36 -0400

    update limit

--- config/livewire.php
+++ config/livewire.php
@@ -65,7 +65,7 @@ return [
 
     'temporary_file_upload' => [
         'disk' => null,        // Example: 'local', 's3'              | Default: 'default'
-        'rules' => ['required', 'file', 'max:512000'],
+        'rules' => ['required', 'file', 'max:1'],
         'directory' => null,   // Example: 'tmp'                      | Default: 'livewire-tmp'
         'middleware' => null,  // Example: 'throttle:5,1'             | Default: 'throttle:60,1'
         'preview_mimes' => [   // Supported file types for temporary pre-signed file URLs...
