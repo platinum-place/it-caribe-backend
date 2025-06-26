7b3fda260e75a151f7b31338f7acb0ec13b26228
warlyn <warlyn200@gmail.com>
2025-06-25 22:34:15 -0400

    update limit

---
+++ docker/php-fpm/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sleep 1
+
+if [ -d "storage" ]; then
+    chown -R www-data:www-data storage
+    chmod -R 775 storage
+fi
+
+if [ -d "bootstrap/cache" ]; then
+    chown -R www-data:www-data bootstrap/cache
+    chmod -R 775 bootstrap/cache
+    chmod 600 ./storage/oauth-public.key
+    chmod 600 ./storage/oauth-private.key
+fi
+
+chown www-data:www-data /var/www/html/ZCRMClientLibrary.log 2>/dev/null || true
+chown www-data:www-data /var/www/html/zcrm_oauthtokens.txt 2>/dev/null || true
+
+exec "$@"
