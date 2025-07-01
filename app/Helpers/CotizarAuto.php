0be4fc1bc72bfeaac64a16af490f5324429dee13
Warlyn <warlyn200@gmail.com>
2025-07-01 11:10:38 -0400

    update

--- app/Helpers/CotizarAuto.php
+++ app/Helpers/CotizarAuto.php
@@ -4,6 +4,8 @@ namespace App\Helpers;
 
 class CotizarAuto extends Cotizar
 {
+    public $prima_minima;
+
     private function uso_restringido($Restringir_veh_culos_de_uso): string
     {
         if (in_array($this->cotizacion->uso, $Restringir_veh_culos_de_uso)) {
@@ -41,19 +43,23 @@ class CotizarAuto extends Cotizar
     private function calcular_tasa($planid)
     {
         $valortasa = 0;
+        $this->prima_minima = false;
         $criterio = "Plan:equals:$planid";
         $tasas = $this->zoho->searchRecordsByCriteria('Tasas', $criterio);
         $tasasValidas = [];
 
         // Filtrar tasas válidas
         foreach ((array) $tasas as $tasa) {
+                        if (
+                ! empty($tasa->getFieldValue('Tipo')) &&
+                $this->cotizacion->plan == $tasa->getFieldValue('Tipo')
+            ) {
+                continue;
+            }
             if (
                 ! empty($tasa->getFieldValue('Grupo_de_veh_culo')) and
                 ! in_array($this->cotizacion->modelotipo, $tasa->getFieldValue('Grupo_de_veh_culo'))
             ) {
-                continue;
-            }
-            if ($this->cotizacion->plan != $tasa->getFieldValue('Tipo')) {
                 continue;
             }
             if (
@@ -73,6 +79,14 @@ class CotizarAuto extends Cotizar
                 $this->cotizacion->suma < $tasa->getFieldValue('Suma_limite')
             ) {
                 continue;
+            }
+            if (
+                ! empty($tasa->getFieldValue('Riesgo')) &&
+                $this->cotizacion->tipo_equipo == $tasa->getFieldValue('Riesgo')
+            ) {
+                continue;
+            }else{
+                        $this->prima_minima = true;
             }
 
             $tasasValidas[] = $tasa;
@@ -233,6 +247,11 @@ class CotizarAuto extends Cotizar
                     }
 
                     $prima = $prima + $cobertura->getFieldValue('Valor_asistencia_vial');
+
+
+                    if($this->prima_minima){
+$prima = $this->prima_minima;
+                    }
                 }
             }
 
