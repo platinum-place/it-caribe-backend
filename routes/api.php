31281e107c672e0dee12b674f4c83868fe282c86
warlyn <warlyn200@gmail.com>
2025-07-02 23:01:45 -0400

    add vehicle resource routes: include VehicleMake, VehicleModel, and VehicleType with restore functionality

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthenticateController;
 use App\Http\Controllers\Partners\VendorController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\Vehicle\VehicleMakeController;
+use App\Http\Controllers\Vehicle\VehicleModelController;
+use App\Http\Controllers\Vehicle\VehicleTypeController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
@@ -9,7 +13,17 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::put('users/{id}/restore', [UserController::class, 'restore'])->name('users.restore');
 
     Route::apiResource('vendors', VendorController::class);
-    Route::put('vendors/{id}/restore', [VendorController::class, 'restore'])->name('users.restore');
+    Route::put('vendors/{id}/restore', [VendorController::class, 'restore'])->name('vendors.restore');
+
+    Route::apiResource('vehicle-makes', VehicleMakeController::class);
+    Route::put('vehicle-makes/{id}/restore', [VehicleMakeController::class, 'restore'])->name('vehicle-makes.restore');
+
+    Route::apiResource('vehicle-models', VehicleModelController::class);
+    Route::put('vehicle-models/{id}/restore', [VehicleModelController::class, 'restore'])->name('vehicle-models.restore');
+
+    Route::apiResource('vehicle-types', VehicleTypeController::class);
+    Route::put('vehicle-types/{id}/restore', [VehicleTypeController::class, 'restore'])->name('vehicle-types.restore');
+
 });
 
 require_once __DIR__.'/api/auth.php';
