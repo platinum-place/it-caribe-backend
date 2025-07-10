d8d2773041583d7cfdcde8cfaba89b0a48473370
warlyn <warlyn200@gmail.com>
2025-07-09 21:32:19 -0400

    feat: update entrypoint script to adjust permissions for storage directories

--- docker/php-fpm/entrypoint.sh
+++ docker/php-fpm/entrypoint.sh
@@ -2,15 +2,11 @@
 set -e
 
 # Ensure correct permissions for Laravel directories
-chmod -R 775 /var/www/html/storage
+chmod -R 600 /var/www/html/storage
+chmod -R 775 /var/www/html/storage/app
+chmod -R 775 /var/www/html/storage/framework
+chmod -R 775 /var/www/html/storage/logs
 chmod -R 775 /var/www/html/bootstrap/cache
-
-if [ -f /var/www/html/storage/oauth-private.key ]; then
-    chmod 600 /var/www/html/storage/oauth-private.key
-fi
-if [ -f /var/www/html/storage/oauth-public.key ]; then
-    chmod 600 /var/www/html/storage/oauth-public.key
-fi
 
 # Start PHP-FPM
 exec php-fpm
