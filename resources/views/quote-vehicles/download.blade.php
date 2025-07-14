4229eac7262d84c6faf6c5ec75c13d88065e1100
Warlyn <warlyn.garcia@bitboxonline.com.do>
2025-07-14 12:01:58 -0400

    feat: update download view for QuoteVehicle; enhance layout and styling for better presentation

--- resources/views/quote-vehicles/download.blade.php
+++ resources/views/quote-vehicles/download.blade.php
@@ -50,51 +50,83 @@
 <table>
     <tbody>
     <tr>
-        <td style="text-align:left;">Ramo/Producto:</td>
+        <td style="text-align:left; font-weight: bold;">Ramo/Producto:</td>
         <td style="text-align:left;">{{ $cotizacion->getFieldValue('Plan') }}</td>
-        <td style="text-align:center;">Correo:</td>
+        <td style="text-align:center; font-weight: bold;">Correo:</td>
         <td style="text-align:center;">{{ $cotizacion->getFieldValue('Correo_electr_nico') }}</td>
-        <td style="text-align:right;">Fecha:</td>
+        <td style="text-align:right; font-weight: bold;">Fecha:</td>
         <td style="text-align:right;">{{ date('d/m/Y', strtotime($cotizacion->getCreatedTime())) }}</td>
     </tr>
-
     <tr>
-        <td style="text-align:left;">Cliente:</td>
+        <td style="text-align:left; font-weight: bold;">Cliente:</td>
         <td style="text-align:left;">{{ $cotizacion->getFieldValue("Nombre") . " " . $cotizacion->getFieldValue("Apellido") }}</td>
-        <td style="text-align:center;">Equipamientos:</td>
+        <td style="text-align:center; font-weight: bold;">Equipamientos:</td>
         <td style="text-align:center;">{{ 'NINGUNO' }}</td>
-        <td style="text-align:right;">Cédula/Pasaporte:</td>
+        <td style="text-align:right; font-weight: bold;">Cédula/Pasaporte:</td>
         <td style="text-align:right;">{{ $cotizacion->getFieldValue('RNC_C_dula') }}</td>
     </tr>
-
     <tr>
-        <td style="text-align:left;">Dirección:</td>
+        <td style="text-align:left; font-weight: bold;">Dirección:</td>
         <td style="text-align:left;"><p>{{ $cotizacion->getFieldValue('Direcci_n') }}</p></td>
-        <td style="text-align:center;">Uso:</td>
+        <td style="text-align:center; font-weight: bold;">Uso:</td>
         <td style="text-align:center;">{{ $cotizacion->getFieldValue('Uso') }}</td>
-        <td style="text-align:right;">Teléfono:</td>
+        <td style="text-align:right; font-weight: bold;">Teléfono:</td>
         <td style="text-align:right;">{{ $cotizacion->getFieldValue('Tel_Residencia') }}</td>
     </tr>
-
     <tr>
-        <td style="text-align:left;">Tipo de vehículo:</td>
+        <td style="text-align:left; font-weight: bold;">Tipo de vehículo:</td>
         <td style="text-align:left;">{{ $cotizacion->getFieldValue('Tipo_veh_culo') }}</td>
-        <td style="text-align:center;">Marca:</td>
+        <td style="text-align:center; font-weight: bold;">Marca:</td>
         <td style="text-align:center;">{{ $cotizacion->getFieldValue('Marca')->getLookupLabel() }}</td>
-        <td style="text-align:right;">Modelo:</td>
+        <td style="text-align:right; font-weight: bold;">Modelo:</td>
         <td style="text-align:right;">{{ $cotizacion->getFieldValue('Modelo')->getLookupLabel() }}</td>
     </tr>
-
     <tr>
-        <td style="text-align:left;">Año:</td>
+        <td style="text-align:left; font-weight: bold;">Año:</td>
         <td style="text-align:left;">{{ $cotizacion->getFieldValue('A_o') }}</td>
-        <td style="text-align:center;">Chasis:</td>
+        <td style="text-align:center; font-weight: bold;">Chasis:</td>
         <td style="text-align:center;">{{ $cotizacion->getFieldValue('Chasis') }}</td>
-        <td style="text-align:right;">Valor asegurado:</td>
+        <td style="text-align:right; font-weight: bold;">Valor asegurado:</td>
         <td style="text-align:right;">{{ number_format($cotizacion->getFieldValue("Suma_asegurada"), 2) }}</td>
     </tr>
     </tbody>
 </table>
 
+<table style="width: 100%; border: 1px solid #000; border-collapse: collapse;">
+    <tr>
+        <td style="font-weight: bold;">Coberturas</td>
+        @foreach ($cotizacion->getLineItems() as $lineItem)
+            @if ($lineItem->getNetTotal() > 0)
+                @php
+                    $product = $libreria->getRecord("Products", $lineItem->getProduct()->getEntityId());
+                    $vendor = $libreria->getRecord("Vendors", $product->getFieldValue('Vendor_Name')->getEntityId());
+                @endphp
+                <td>
+                    {{ $vendor->getFieldValue('Nombre') }}
+                </td>
+            @endif
+        @endforeach
+    </tr>
+{{--    <tr>--}}
+{{--        <td style="font-weight: bold;">Prima Neta</td>--}}
+{{--        @foreach ($cotizacion->getLineItems() as $lineItem)--}}
+{{--            @if ($lineItem->getNetTotal() > 0)--}}
+{{--                <td>--}}
+{{--                    RD$ {{ number_format($lineItem->getNetTotal() / 1.16, 2) }}--}}
+{{--                </td>--}}
+{{--            @endif--}}
+{{--        @endforeach--}}
+{{--    </tr>--}}
+{{--    <tr>--}}
+{{--        <td style="font-weight: bold;">Prima Total</td>--}}
+{{--        @foreach ($cotizacion->getLineItems() as $lineItem)--}}
+{{--            @if ($lineItem->getNetTotal() > 0)--}}
+{{--                <td>--}}
+{{--                    RD$ {{ number_format($lineItem->getNetTotal(), 2) }}--}}
+{{--                </td>--}}
+{{--            @endif--}}
+{{--        @endforeach--}}
+{{--    </tr>--}}
+</table>
 </body>
 </html>
