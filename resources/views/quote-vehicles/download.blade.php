a084008e3ae3d830469f02426b556fe640271422
laravel <laravel@localhost>
2025-07-24 10:50:25 -0400

    feat: consolidate Docker configurations; remove php-cli and supervisord images; unify worker setup; streamline service definitions in docker-compose configuration

--- resources/views/quote-vehicles/download.blade.php
+++ resources/views/quote-vehicles/download.blade.php
@@ -252,14 +252,14 @@
     </tr>
 
     <tr>
-        <td style="border: none; font-weight: bold;">Prima Anual y ISC</td>
+        <td style="border: none; font-weight: bold;">Prima Anual i/ISC</td>
         @foreach ($lineItemsData as $data)
             <td>{{ number_format($data['netTotal'], 2) }}</td>
         @endforeach
     </tr>
 
     <tr>
-        <td style="border: none; font-weight: bold;">Prima Mensual y ISC</td>
+        <td style="border: none; font-weight: bold;">Prima Mensual i/ISC</td>
         @foreach ($lineItemsData as $data)
             <td>{{ number_format($data['monthlyTotal'], 2) }}</td>
         @endforeach
