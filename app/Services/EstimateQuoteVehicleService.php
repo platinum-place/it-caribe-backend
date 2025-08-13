40b78bf85d7c34929db04f6ea08e3a7618b974a1
laravel <laravel@localhost>
2025-08-13 14:27:34 -0400

    update filament

--- app/Services/EstimateQuoteVehicleService.php
+++ app/Services/EstimateQuoteVehicleService.php
@@ -71,9 +71,9 @@ class EstimateQuoteVehicleService
                 }
             }
 
-//            if ($shouldSkip) {
-//                continue;
-//            }
+            if ($shouldSkip) {
+                continue;
+            }
 
             $rate = $this->getRate($product['id'], $vehicleAmount, $vehicleYear, $vehicleType);
 
@@ -100,9 +100,9 @@ class EstimateQuoteVehicleService
                 }
             }
 
-//            if ($rate == 0 && $error == '') {
-//                continue;
-//            }
+            if ($rate == 0 && $error == '') {
+                continue;
+            }
 
             $amount = round($amount, 2);
             $amountTaxed = round($amountTaxed, 2);
