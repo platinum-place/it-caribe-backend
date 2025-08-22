1716d6c748a0fb4679d76bada5bb3d3a4385796d
warlyn <warlyn200@gmail.com>
2025-08-22 11:51:50 -0400

    commit

--- app/Services/EstimateQuoteUnemploymentService.php
+++ app/Services/EstimateQuoteUnemploymentService.php
@@ -51,7 +51,7 @@ class EstimateQuoteUnemploymentService
                 }
             } else {
                 if (!empty($product['Indemnizaci_n'])) {
-                    $amount = $loanInstallment * ($rate / 100) * $product['Indemnizaci_n'] * $deadline;
+                    $amount = $loanInstallment * $product['Indemnizaci_n'] * $deadline / 1000 * 9.38
                 } else {
                     $amount = $loanInstallment * ($rate / 100) * $deadline;
                 }
