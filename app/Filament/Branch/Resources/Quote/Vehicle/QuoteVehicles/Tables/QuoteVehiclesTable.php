74da97d4bc8843b91457b74311e8e450b2a7981f
warlyn <warlyn200@gmail.com>
2025-08-29 10:34:44 -0400

    commit

--- app/Filament/Branch/Resources/Quote/Vehicle/QuoteVehicles/Tables/QuoteVehiclesTable.php
+++ app/Filament/Branch/Resources/Quote/Vehicle/QuoteVehicles/Tables/QuoteVehiclesTable.php
@@ -20,42 +20,65 @@ class QuoteVehiclesTable
     public static function configure(Table $table): Table
     {
         return $table
+            ->defaultSort('created_at', 'desc')
             ->columns([
-                TextColumn::make('created_at')
-                    ->dateTime()
+                TextColumn::make('quote.start_date')
                     ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
-                TextColumn::make('updated_at')
-                    ->dateTime()
+                    ->label('FECHA DE EMISIÓN'),
+
+                TextColumn::make('quote.lead.full_name')
+                    ->searchable()
+                    ->label('NOMBRE'),
+
+                TextColumn::make('quote.lead.identity_number')
+                    ->searchable()
+                    ->label('IDENTIFICADOR'),
+
+                TextColumn::make('quote.lead.birth_date')
+                    ->label('FECHA DE NAC'),
+
+                TextColumn::make('quote.lead.age')
+                    ->label('EDAD'),
+
+                TextColumn::make('quote.end_date')
                     ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
-                TextColumn::make('deleted_at')
-                    ->dateTime()
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
-                TextColumn::make('created_by')
-                    ->numeric()
-                    ->sortable(),
-                TextColumn::make('updated_by')
-                    ->numeric()
-                    ->sortable(),
-                TextColumn::make('deleted_by')
-                    ->numeric()
-                    ->sortable(),
-                TextColumn::make('quote.id')
-                    ->searchable(),
-                TextColumn::make('vehicle.id')
-                    ->searchable(),
+                    ->label('VENCIMIENTO'),
+
+                TextColumn::make('quote.selectedLine.name')
+                    ->label('ASEGURADORA'),
+
+                TextColumn::make('selectedLine.total_monthly')
+                    ->label('PRIMA COBRADA CLIENTE (INCLUYE VIDA + MARKUP)'),
+
+                TextColumn::make('selectedLine.amount_without_life_amount')
+                    ->label('PRIMA VEHICULO (SIN VIDA Y MARKUP)'),
+
+                TextColumn::make('selectedLine.life_amount')
+                    ->label('PRIMA VIDA'),
+
+                TextColumn::make('selectedLine.markup')
+                    ->label('MARKUP VEHICULO'),
+
+                TextColumn::make('quote.selectedLine.total')
+                    ->label('PRIMA A PAGAR ASEGURADORA VEH.'),
+
                 TextColumn::make('vehicle_amount')
-                    ->numeric()
-                    ->sortable(),
-                TextColumn::make('vehicle_loan_amount')
-                    ->numeric()
-                    ->sortable(),
-                IconColumn::make('is_employee')
-                    ->boolean(),
-                IconColumn::make('leasing')
-                    ->boolean(),
+                    ->label('VALOR ASEGURADO'),
+
+                TextColumn::make('vehicle.chassis')
+                    ->label('CHASIS'),
+
+                TextColumn::make('vehicle.vehicleMake.name')
+                    ->label('MARCA'),
+
+                TextColumn::make('vehicle.vehicleModel.name')
+                    ->label('MODELO'),
+
+                TextColumn::make('vehicle.vehicle_year')
+                    ->label('Año'),
+
+                TextColumn::make('vehicle.vehicleUtility.name')
+                    ->label('PLAN'),
             ])
             ->filters([
                 TrashedFilter::make(),
